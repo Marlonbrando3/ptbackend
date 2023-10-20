5c05c6e6fc9d8ad7d845febf20150b3a70fb7bbf
Marek <marek@MacBook-Air-Marek.local>
2023-10-20 08:29:34 +0200

    first commit

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const PORT = 8080;
 const cors = require("cors");
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/api/home", (req, res) => {
   res.json({ msg: "Hello !" });
@@ -15,8 +17,8 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-  // res.send({ msg: "I've recived a data!" });
-  res.json({ msg: "I've recived a data" });
+  res.json({ msg: req.body });
+  console.log("I've recived a data");
 });
 
 app.listen(PORT, () => {
